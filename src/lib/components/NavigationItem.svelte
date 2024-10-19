0b671f38901ee14c54c6ee0cc606803e63481b23
Reiner Knudsen <reinerknudsen@me.com>
2024-10-19 18:34:58 +0200

    init

---
+++ src/lib/components/NavigationItem.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let url;
+  export let label;
+  export let mode;
+  export let menuObjects;
+</script>
+
+<li>
+  <a
+    href={url}
+    class="md:hover:text-primary-700 md:dark:hover:text-white dark:hover:text-white block rounded py-2 pe-4 ps-3 text-xl text-primary-100 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 md:border-0 md:p-0 md:hover:bg-transparent md:dark:hover:bg-transparent"
+    >{label}</a
+  >
+</li>

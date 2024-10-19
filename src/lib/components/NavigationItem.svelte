251426f1de0363004a77cfe82788c6534944555d
Reiner Knudsen <reinerknudsen@me.com>
2024-10-19 20:23:39 +0200

    Made menu more flexible through components

--- src/lib/components/NavigationItem.svelte
+++ src/lib/components/NavigationItem.svelte
@@ -1,14 +1,12 @@
-<script>
-  export let url;
-  export let label;
-  export let mode;
-  export let menuObjects;
+<script lang="ts">
+	export let url;
+	export let label;
 </script>
 
 <li>
-  <a
-    href={url}
-    class="md:hover:text-primary-700 md:dark:hover:text-white dark:hover:text-white block rounded py-2 pe-4 ps-3 text-xl text-primary-100 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 md:border-0 md:p-0 md:hover:bg-transparent md:dark:hover:bg-transparent"
-    >{label}</a
-  >
+	<a
+		href={url}
+		class="md:hover:text-primary-700 md:dark:hover:text-white dark:hover:text-white block rounded py-2 pe-4 ps-3 text-xl text-primary-100 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 md:border-0 md:p-0 md:hover:bg-transparent md:dark:hover:bg-transparent"
+		>{label}</a
+	>
 </li>
---
+++ src/lib/components/NavigationRollUp.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import type { MenuItem } from '$lib/types';
+
+	export let title: string;
+	export let menuItems: MenuItem[];
+
+	let showDropdown = false;
+
+	const toggleDropdown = () => {
+		showDropdown = !showDropdown;
+	};
+</script>
+
+<div>
+	<li class="group relative">
+		<!-- svelte-ignore a11y-invalid-attribute -->
+		<button class="text-xl" on:click|preventDefault={toggleDropdown}>{title}</button>
+		{#if showDropdown}
+			<div
+				class="ring-black absolute left-0 mt-2 w-48 rounded-md bg-white-primary shadow-lg ring-1 ring-opacity-5"
+			>
+				{#each menuItems as menuItem}
+					<div class="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+						<a
+							href={menuItem.url}
+							class="block px-4 py-2 text-sm text-primary-100 hover:bg-gray-100 hover:text-gray-900"
+							role="menuitem"
+							on:click={toggleDropdown}>{menuItem.title}</a
+						>
+					</div>
+				{/each}
+			</div>
+		{/if}
+	</li>
+</div>

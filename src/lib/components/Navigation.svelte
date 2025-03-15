7ab54f6d28eb0780b015b1dce17bd51f1989e902
Reiner Knudsen <reinerknudsen@me.com>
2025-03-15 18:42:21 +0100

    Mobile Menu enabled an adjusted

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,20 +1,18 @@
 <script lang="ts">
 	import { authStore, unloadUser } from '$lib/stores/AuthStore';
-	import { auth, settingsColRef } from '$lib/firebase/firebaseConfig';
+	import { auth } from '$lib/firebase/firebaseConfig';
 	import { goto } from '$app/navigation';
 	import { signOut } from 'firebase/auth';
 	import caplogo from '$lib/assets/chaplaincy_logo.png';
 	import { about as aboutItems } from '$lib/data/data.json';
 
-	import type { MenuItem } from '$lib/types';
-
-	import { page } from '$app/stores';
 	import NavigationItem from '$lib/components/NavigationItem.svelte';
 	import NavigationRollUp from '$lib/components/NavigationRollUp.svelte';
 
 	let user;
+	let menuOpen = true;
+	let aboutMenuOpen = true;
 
-	$: activeUrl = $page.url.pathname;
 	$: authStore.subscribe((store) => {
 		user = store;
 	});
@@ -28,47 +26,124 @@
 		} catch (error) {
 			console.error('Error signing out:', error);
 		}
+		if (menuOpen) toggleMobileMenu;
 	};
 
-	const textSizeMenu = 'text-xl';
+	const toggleMobileMenu = () => {
+		menuOpen = !menuOpen;
+	};
+
+	const toggleAboutMenu = () => {
+		aboutMenuOpen = !aboutMenuOpen;
+	};
+
+	const handleLogin = () => {
+		goto('/login');
+		toggleMobileMenu();
+	};
 </script>
 
 <nav
-	class="sticky top-0 z-50 flex h-full min-h-28 w-full items-center divide-gray-100 rounded-b-2xl border-b border-gray-100 bg-white-primary px-2 py-2.5 align-middle text-gray-700 shadow-xl dark:divide-gray-700 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-200 sm:px-4"
+	class="sticky top-0 z-50 flex h-full min-h-28 w-full items-center justify-between divide-gray-100 rounded-b-2xl border-b border-gray-100 bg-white-primary px-4 py-2.5 text-gray-700 shadow-xl dark:divide-gray-700 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-200"
 >
-	<div class="container mx-auto flex flex-wrap items-center justify-between">
+	<!-- Logo -->
+	<div class="nav-container flex w-full justify-between">
 		<a href="/" class="flex items-center">
-			<img class="me-3 sm:h-9" src={caplogo} alt="Chaplaincy Logo" />
+			<img class="logo-image me-3 h-9 lg:h-[70%]" src={caplogo} alt="Chaplaincy Logo" />
 		</a>
-		<button
-			type="button"
-			id="mobile-menu"
-			class="m-0.5 ms-3 whitespace-normal rounded-lg p-1.5 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-400 dark:hover:bg-gray-600 md:hidden"
-			aria-label="Open main menu"
-			><span class="sr-only">Open main menu</span>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				role="button"
-				tabindex="0"
-				width="24"
-				height="24"
-				class="h-6 w-6 shrink-0"
-				aria-label="bars 3"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="2"
+
+		<!-- Mobile Menu -->
+		<div class="mobile-menu-container lg:hidden">
+			<button
+				type="button"
+				id="mobile-menu"
+				class="m-0.5 ms-3 whitespace-normal rounded-lg p-1.5 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-400 dark:hover:bg-gray-600 lg:hidden"
+				aria-label="Open main menu"
+				on:click={toggleMobileMenu}
+				><span class="sr-only">Open main menu</span>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					role="button"
+					tabindex="0"
+					width="24"
+					height="24"
+					class="h-6 w-6 shrink-0"
+					aria-label="bars 3"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="2"
+				>
+					<path
+						stroke="currentColor"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M4 6h16M4 12h16M4 18h16"
+					></path>
+				</svg></button
 			>
-				<path
-					stroke="currentColor"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M4 6h16M4 12h16M4 18h16"
-				></path>
-			</svg></button
-		>
-		<div class="menu hidden w-full cursor-pointer md:block md:w-auto">
+			{#if menuOpen}
+				<div
+					class="mobile-menu absolute left-[20%] top-28 z-20 w-[80%] cursor-pointer rounded-lg bg-white-primary px-4 py-4"
+				>
+					<ul class="gap-2p-4 mt-4 flex flex-col text-primary-100">
+						<NavigationItem url="/" label="Home" onClick={toggleMobileMenu} />
+						<NavigationItem url="/worship" label="Worship" onClick={toggleMobileMenu} />
+						<NavigationItem url="/news" label="News" onClick={toggleMobileMenu} />
+						<NavigationItem url="/activities" label="Activities" onClick={toggleMobileMenu} />
+						<NavigationItem url="/groups" label="Groups" onClick={toggleMobileMenu} />
+						<NavigationItem url="#" label="About us" onClick={toggleAboutMenu} />
+						{#if aboutMenuOpen}
+							<div class="pl-4">
+								<NavigationItem url="/about" label="Who we are" onClick={toggleMobileMenu} />
+								<NavigationItem
+									url="/about/responsibilities"
+									label="Who is who"
+									onClick={toggleMobileMenu}
+								/>
+								<NavigationItem
+									url="/about/locations"
+									label="Where we are"
+									onClick={toggleMobileMenu}
+								/>
+								<NavigationItem
+									url="/about/safeguarding"
+									label="Safeguarding"
+									onClick={toggleMobileMenu}
+								/>
+								<NavigationItem
+									url="/about/contact"
+									label="Get in touch"
+									onClick={toggleMobileMenu}
+								/>
+							</div>
+						{/if}
+
+						{#if $authStore.role === 'admin' || $authStore.role === 'editor'}
+							<hr class="mb-4" />
+							<NavigationItem url="/admin" label="Admin" />
+						{/if}
+					</ul>
+					<div class="flex flex-col">
+						{#if !$authStore.isLoggedIn}
+							<button
+								class="text-md rounded-lg bg-primary-20 py-2 text-primary-100 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 md:text-xl"
+								on:click={handleLogin}>Login</button
+							>
+						{:else}
+							<button
+								class="text-md rounded-lg bg-primary-20 py-2 text-primary-100 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 md:text-xl"
+								on:click={handleLogout}>Sign out</button
+							>
+						{/if}
+					</div>
+				</div>
+			{/if}
+		</div>
+
+		<!-- Desktop Menu -->
+		<div class="menu hidden w-full cursor-pointer lg:block lg:w-auto">
 			<ul
-				class="mt-4 flex flex-col p-4 text-primary-100 md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium"
+				class="mt-4 flex flex-col p-4 text-primary-100 lg:mt-0 lg:flex-row lg:space-x-8 lg:text-sm lg:font-medium"
 			>
 				<NavigationItem url="/" label="Home" />
 				<NavigationItem url="/worship" label="Worship" />
@@ -84,7 +159,7 @@
 				{:else}
 					<li>
 						<button
-							class="md:hover:text-primary-700 md:dark:hover:text-white dark:hover:text-white block rounded py-2 pe-4 ps-3 text-xl text-primary-100 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 md:border-0 md:p-0 md:hover:bg-transparent md:dark:hover:bg-transparent"
+							class="lg:hover:text-primary-700 lg:dark:hover:text-white dark:hover:text-white block rounded py-2 pe-4 ps-3 text-xl text-primary-100 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 lg:border-0 lg:p-0 lg:hover:bg-transparent lg:dark:hover:bg-transparent"
 							on:click={handleLogout}>Sign out</button
 						>
 					</li>

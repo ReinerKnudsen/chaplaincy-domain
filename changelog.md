cf7243782297e5c58af75c60fdc226e593bf9949
Reiner Knudsen <rknudsen@conet.de>
2025-06-24 15:13:40 +0200

    removed dependencies from formats.ts, updated formatting of news and events article view

---
+++ changelog.md
@@ -0,0 +1,55 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+### Added
+
+-
+
+### Changed
+
+- removed dependency on formats.ts
+- added multiple styles to the app.css file
+- adjusted news and article overview pages to use the new styles
+
+### Fixed
+
+-
+
+## [1.0.0] - 2025-06-24
+
+### Added
+
+- Initial release of the chaplaincy domain application
+- User authentication and login system
+- News management functionality
+- Events management functionality
+- Basic responsive UI design
+
+### Changed
+
+-
+
+### Deprecated
+
+-
+
+### Removed
+
+-
+
+### Fixed
+
+-
+
+### Security
+
+-
+
+[Unreleased]: https://github.com/username/chaplaincy-domain/compare/v1.0.0...HEAD
+[1.0.0]: https://github.com/username/chaplaincy-domain/releases/tag/v1.0.0
--- src/routes/(pages)/events/[eventId]/+page.svelte
+++ src/routes/(pages)/events/[eventId]/+page.svelte
@@ -4,8 +4,6 @@
 	import { marked } from 'marked';
 	import Icon from '@iconify/svelte';
 
-	import * as formats from '$lib/formats';
-
 	import { loadItem, CollectionType, EventStore } from '$lib/stores/ObjectStore';
 	import { fetchLocations, type Location, AllLocations } from '$lib/stores/LocationsStore';
 
@@ -33,22 +31,22 @@
 {#if loading}
 	<p>Loading...</p>
 {:else if $EventStore}
-	<div class="bg-white-primary rounded-2xl">
-		<div class={`container ${formats.container}`}>
-			<div class={`headline ${formats.headline}`}>
+	<div id="article-container"class="article-container">
+		<div id="article-content">
+			<div id="article-title" class="article-title">
 				{$EventStore.title}
 			</div>
-			<div class={`event-data ${formats.itemMetaData}`}>
-				<div class={`entry ${formats.itemMetaDataEntry}`}>
+			<div id="event-data" class= "article-metadata">
+				<div id="article-date" class="article-metadata-item">
 					<Icon icon="fa6-regular:calendar" />
 					{$EventStore.startdate}
 				</div>
-				<div class={`entry ${formats.itemMetaDataEntry}`}>
+				<div id="article-time" class="article-metadata-item">
 					<Icon icon="fa6-regular:clock" />
 					{$EventStore.starttime}
 				</div>
 				{#if $EventStore.location}
-					<div class={`entry ${formats.itemMetaDataEntry}`}>
+					<div id="article-location" class="article-metadata-item">
 						<Icon icon="gis:location-poi" />
 						<a class="link" target="_blank" href={location?.openMapUrl}
 							>{`${location?.name}, ${location?.city}`}</a
@@ -57,18 +55,23 @@
 				{/if}
 			</div>
 			{#if $EventStore.image}
-				<div class={`event-image ${formats.itemImageContainer}`}>
-					<img class={formats.itemImage} src={$EventStore.image} alt={$EventStore.title} />
+				<div id="article-image" class="article-image-container">
+					<img class="article-image" src={$EventStore.image} alt={$EventStore.title} />
 				</div>
 			{/if}
 			{#if description}
 				<MarkdownViewer content={description} />
 			{/if}
 		</div>
+		<div id="pdfFile">
+			{#if $EventStore.pdfFile}
+				Download PDF
+			{/if}
+		</div>
 	</div>
-	<div class={`back-link ${formats.backLink}`}>
+	<div class="back-link">
 		<Icon icon="fa6-regular:circle-left" class="h-6 w-6" />
-		<a class={formats.aLink} href="/events">Take me back to overview</a>
+		<a class="link" href="/events">Take me back to overview</a>
 	</div>
 {:else}
 	<p>Event not found</p>

00261548a21dbde803d7f5590eae2104ed9e0a4a
Reiner Knudsen <reinerknudsen@me.com>
2025-06-26 19:46:38 +0200

    unit test for validateForm complete

--- src/lib/services/validateForm.ts
+++ src/lib/services/validateForm.ts
@@ -2,89 +2,84 @@ import { type DomainEvent } from '$lib/stores/ObjectStore';
 import { notificationStore } from '$lib/stores/notifications';
 
 interface FormError {
-	message: string;
+  message: string;
 }
 
 export interface FormData {
-	target: Record<string, { name: string; required: boolean; value: string }>;
+  target: Record<string, { name: string; required: boolean; value: string }>;
 }
 
 export function validateEmptyInput(formData: FormData): string[] {
-	if (!formData.target) {
-		return [];
-	}
-	const errObject = [];
-	for (const field in formData.target) {
-		if (formData.target[field].required && formData.target[field].value === '') {
-			errObject.push(formData.target[field].name);
-		}
-	}
-	return errObject;
+  if (!formData.target) {
+    return [];
+  }
+  const errObject = [];
+  for (const field in formData.target) {
+    if (formData.target[field].required && formData.target[field].value === '') {
+      errObject.push(formData.target[field].name);
+    }
+  }
+  return errObject;
 }
 
 const buildTimeStamp = (date: string, time: string): Date => {
-	return new Date(`${date}T${time}Z`);
+  return new Date(`${date}T${time}Z`);
 };
 
 export function validateEventData(event: DomainEvent): FormError[] {
-	//const event = get(EventStore);
-	const formErrors: FormError[] = [];
+  //const event = get(EventStore);
+  const formErrors: FormError[] = [];
 
-	if (!event) {
-		throw new Error('Event could be found');
-	}
-	const {
-		startdate,
-		starttime,
-		enddate,
-		endtime,
-		publishdate,
-		publishtime,
-		unpublishdate,
-		unpublishtime,
-	} = event;
+  if (!event) {
+    throw new Error('Event could be found');
+  }
+  const {
+    startdate,
+    starttime,
+    enddate,
+    endtime,
+    publishdate,
+    publishtime,
+    unpublishdate,
+    unpublishtime,
+  } = event;
 
-	if (!startdate) {
-		notificationStore.addToast('error', 'Mandatory start date is empty');
-	}
+  const startDateTime = starttime && startdate && buildTimeStamp(startdate, starttime).getTime();
+  const endDateTime = enddate && endtime && buildTimeStamp(enddate, endtime).getTime();
+  const publishDateTime =
+    publishdate && publishtime && buildTimeStamp(publishdate, publishtime).getTime();
+  const unpublishDateTime =
+    unpublishdate && unpublishtime && buildTimeStamp(unpublishdate, unpublishtime).getTime();
 
-	if (!starttime) {
-		notificationStore.addToast('error', 'Mandatory start time is empty');
-	}
+  if (!startdate) {
+    notificationStore.addToast('error', 'Mandatory start date is empty');
+  } else if (!starttime) {
+    notificationStore.addToast('error', 'Mandatory start time is empty');
+  }
 
-	if (new Date(startdate) < new Date()) {
-		notificationStore.addToast('error', 'The start date cannot be in the past');
-	}
+  if (new Date(startdate) < new Date()) {
+    notificationStore.addToast('error', 'The start date cannot be in the past');
+  }
 
-	if (
-		startdate &&
-		starttime &&
-		enddate &&
-		endtime &&
-		buildTimeStamp(enddate, endtime) < buildTimeStamp(startdate, starttime)
-	) {
-		notificationStore.addToast('error', 'The event end cannot be before the event starts');
-	}
+  if (startDateTime && endDateTime) {
+    if (endDateTime < startDateTime) {
+      notificationStore.addToast('error', 'The event end cannot be before the event start');
+    } else if (endDateTime === startDateTime) {
+      notificationStore.addToast('warning', 'Both start and end date-time are the same');
+    }
+  }
 
-	if (
-		startdate &&
-		starttime &&
-		publishdate &&
-		publishtime &&
-		buildTimeStamp(startdate, starttime) < buildTimeStamp(publishdate, publishtime)
-	) {
-		notificationStore.addToast('error', 'The publish date cannot be after the start date');
-	}
+  if (startDateTime && publishDateTime && startDateTime < publishDateTime) {
+    notificationStore.addToast('error', 'The publish date cannot be after the start date');
+  }
 
-	if (
-		unpublishdate &&
-		unpublishtime &&
-		publishdate &&
-		publishtime &&
-		buildTimeStamp(unpublishdate, unpublishtime) < buildTimeStamp(publishdate, publishdate)
-	) {
-		notificationStore.addToast('error', 'The unpublish date cannot be before the publish date');
-	}
+  if (unpublishDateTime && publishDateTime) {
+    if (unpublishDateTime < publishDateTime) {
+      notificationStore.addToast('error', 'The unpublish date cannot be before the publish date');
+    } else if (unpublishDateTime === publishDateTime) {
+      notificationStore.addToast('warning', 'Both publish and unpublish date-time are the same.');
+    }
 
-	return formErrors;
+    return formErrors;
+  }
 }
--- src/lib/services/validateForms.test.ts
+++ src/lib/services/validateForms.test.ts
@@ -4,197 +4,266 @@ import { notificationStore } from '$lib/stores/notifications';
 
 // Mock the notification store
 vi.mock('$lib/stores/notifications', () => ({
-	notificationStore: {
-		addToast: vi.fn(),
-		addAlert: vi.fn(),
-		// ... other methods if needed
-	},
+  notificationStore: {
+    addToast: vi.fn(),
+    addAlert: vi.fn(),
+    clear: vi.fn(),
+    // ... other methods if needed
+  },
 }));
 
 describe('validateEmptyInput', () => {
-	it('ok: all required fields have values', () => {
-		const formData = {
-			target: {
-				feld1: { name: 'feld1', required: true, value: 'feld1value' },
-				feld2: { name: 'feld2', required: true, value: 'feld2value' },
-			},
-		};
-		expect(validateEmptyInput(formData)).toEqual([]);
-	});
+  it('ok: all required fields have values', () => {
+    const formData = {
+      target: {
+        feld1: { name: 'feld1', required: true, value: 'feld1value' },
+        feld2: { name: 'feld2', required: true, value: 'feld2value' },
+      },
+    };
+    expect(validateEmptyInput(formData)).toEqual([]);
+  });
 
-	it('error: required field without value', () => {
-		const formData = {
-			target: {
-				feld1: { name: 'feld1', required: true, value: 'feld1value' },
-				feld2: { name: 'feld2', required: true, value: '' },
-			},
-		};
-		expect(validateEmptyInput(formData)).toEqual(['feld2']);
-	});
+  it('error: required field without value', () => {
+    const formData = {
+      target: {
+        feld1: { name: 'feld1', required: true, value: 'feld1value' },
+        feld2: { name: 'feld2', required: true, value: '' },
+      },
+    };
+    expect(validateEmptyInput(formData)).toEqual(['feld2']);
+  });
 
-	it('ok: one optional field without value', () => {
-		const formData = {
-			target: {
-				feld1: { name: 'feld1', required: true, value: 'feld1value' },
-				feld2: { name: 'feld2', required: false, value: '' },
-			},
-		};
-		expect(validateEmptyInput(formData)).toEqual([]);
-	});
+  it('ok: one optional field without value', () => {
+    const formData = {
+      target: {
+        feld1: { name: 'feld1', required: true, value: 'feld1value' },
+        feld2: { name: 'feld2', required: false, value: '' },
+      },
+    };
+    expect(validateEmptyInput(formData)).toEqual([]);
+  });
 
-	it('ok: optional field with value && required field without value', () => {
-		const formData = {
-			target: {
-				feld1: { name: 'feld1', required: true, value: '' },
-				feld2: { name: 'feld2', required: false, value: 'feld2value' },
-			},
-		};
-		expect(validateEmptyInput(formData)).toEqual(['feld1']);
-	});
-	it('ok: one optional field with value', () => {
-		const formData = {
-			target: {
-				feld1: { name: 'feld1', required: true, value: 'feld1value' },
-				feld2: { name: 'feld2', required: false, value: 'feld2value' },
-			},
-		};
-		expect(validateEmptyInput(formData)).toEqual([]);
-	});
-	it('ok: no required fields in the form', () => {
-		const formData = {
-			target: {
-				feld1: { name: 'feld1', required: false, value: 'feld1value' },
-				feld2: { name: 'feld2', required: false, value: '' },
-			},
-		};
-		expect(validateEmptyInput(formData)).toEqual([]);
-	});
-	it('ok: empty form', () => {
-		const formData = {
-			target: {},
-		};
-		expect(validateEmptyInput(formData)).toEqual([]);
-	});
+  it('ok: optional field with value && required field without value', () => {
+    const formData = {
+      target: {
+        feld1: { name: 'feld1', required: true, value: '' },
+        feld2: { name: 'feld2', required: false, value: 'feld2value' },
+      },
+    };
+    expect(validateEmptyInput(formData)).toEqual(['feld1']);
+  });
+  it('ok: one optional field with value', () => {
+    const formData = {
+      target: {
+        feld1: { name: 'feld1', required: true, value: 'feld1value' },
+        feld2: { name: 'feld2', required: false, value: 'feld2value' },
+      },
+    };
+    expect(validateEmptyInput(formData)).toEqual([]);
+  });
+  it('ok: no required fields in the form', () => {
+    const formData = {
+      target: {
+        feld1: { name: 'feld1', required: false, value: 'feld1value' },
+        feld2: { name: 'feld2', required: false, value: '' },
+      },
+    };
+    expect(validateEmptyInput(formData)).toEqual([]);
+  });
+  it('ok: empty form', () => {
+    const formData = {
+      target: {},
+    };
+    expect(validateEmptyInput(formData)).toEqual([]);
+  });
 });
 
 describe('validDateEventData', () => {
-	it('ok: happy flow, no problem', () => {
-		const event = {
-			startdate: '2025-09-09',
-			starttime: '13:00',
-			enddate: '2025-09-09',
-			endtime: '18:00',
-			publishdate: '2025-07-09',
-			publishtime: '09:00',
-			unpublishDate: '2025-09-09',
-			unpublishTime: '18:00',
-		};
-		// @ts-expect-error - Testing with incomplete event object
-		const result = validateEventData(event);
-		expect(notificationStore.addToast).not.toHaveBeenCalled();
-		expect(result).toEqual([]); // Still returns empty array for no errors
-	});
+  it('error: no event passed to function', () => {
+    const event = null;
+    expect(() => validateEventData(event)).toThrow('Event could be found');
+  });
 
-	it('error: no startdate for event', () => {
-		const event = {
-			startdate: '',
-			starttime: '13:00',
-		};
-		// @ts-expect-error - Testing with incomplete event object
-		validateEventData(event);
-		expect(notificationStore.addToast).toHaveBeenCalledWith(
-			'error',
-			'Mandatory start date is empty'
-		);
-		expect(notificationStore.addToast).toHaveBeenCalledTimes(1);
-	});
+  it('ok: happy flow, no problem', () => {
+    const event = {
+      startdate: '2025-09-09',
+      starttime: '13:00',
+      enddate: '2025-09-09',
+      endtime: '18:00',
+      publishdate: '2025-07-09',
+      publishtime: '09:00',
+      unpublishdate: '2025-09-09',
+      unpublishtime: '18:00',
+    };
+    // @ts-expect-error - Testing with incomplete event object
+    const result = validateEventData(event);
+    expect(notificationStore.addToast).not.toHaveBeenCalled();
+    expect(result).toEqual([]); // Still returns empty array for no errors
+  });
 
-	it('error: no starttime for event', () => {
-		const event = {
-			startdate: '2025-08-09',
-			starttime: '',
-		};
-		// @ts-expect-error - Testing with incomplete event object
-		validateEventData(event);
-		expect(notificationStore.addToast).toHaveBeenCalledWith(
-			'error',
-			'Mandatory start time is empty'
-		);
-	});
+  it('error: no startdate for event', () => {
+    const event = {
+      startdate: '',
+      starttime: '13:00',
+    };
+    // @ts-expect-error - Testing with incomplete event object
+    validateEventData(event);
+    expect(notificationStore.addToast).toHaveBeenCalledWith(
+      'error',
+      'Mandatory start date is empty'
+    );
+    expect(notificationStore.addToast).toHaveBeenCalledTimes(1);
+  });
 
-	it('error: startdate before today', () => {
-		const yesterday = new Date();
-		yesterday.setDate(yesterday.getDate() - 1);
-		const yesterdayString = yesterday.toISOString().split('T')[0];
-		const event = {
-			startdate: yesterdayString,
-			starttime: '13:00',
-		};
-		// @ts-expect-error - Testing with incomplete event object
-		validateEventData(event);
-		expect(notificationStore.addToast).toHaveBeenCalledWith(
-			'error',
-			'The start date cannot be in the past'
-		);
-	});
+  it('error: no starttime for event', () => {
+    const event = {
+      startdate: '2025-08-09',
+      starttime: '',
+    };
+    // @ts-expect-error - Testing with incomplete event object
+    validateEventData(event);
+    expect(notificationStore.addToast).toHaveBeenCalledWith(
+      'error',
+      'Mandatory start time is empty'
+    );
+  });
 
-	it('error: endDateTime before startDateTime based on enddate', () => {
-		const event = {
-			startdate: '2025-09-09',
-			starttime: '13:00',
-			enddate: '2025-01-01',
-			endtime: '18:00',
-		};
-		// @ts-expect-error - Testing with incomplete event object
-		validateEventData(event);
-		expect(notificationStore.addToast).toHaveBeenCalledWith(
-			'error',
-			'The event end cannot be before the event starts'
-		);
-	});
+  it('error: startdate before today', () => {
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    const yesterdayString = yesterday.toISOString().split('T')[0];
+    const event = {
+      startdate: yesterdayString,
+      starttime: '13:00',
+    };
+    // @ts-expect-error - Testing with incomplete event object
+    validateEventData(event);
+    expect(notificationStore.addToast).toHaveBeenCalledWith(
+      'error',
+      'The start date cannot be in the past'
+    );
+  });
 
-	it('error: endDateTime before startDateTime based on endtime', () => {
-		const event = {
-			startdate: '2025-09-09',
-			starttime: '13:00',
-			enddate: '2025-09-09',
-			endtime: '03:00',
-		};
-		// @ts-expect-error - Testing with incomplete event object
-		validateEventData(event);
-		expect(notificationStore.addToast).toHaveBeenCalledWith(
-			'error',
-			'The event end cannot be before the event starts'
-		);
-	});
+  it('error: endDateTime before startDateTime based on enddate', () => {
+    const event = {
+      startdate: '2025-09-09',
+      starttime: '13:00',
+      enddate: '2025-01-01',
+      endtime: '18:00',
+    };
+    // @ts-expect-error - Testing with incomplete event object
+    validateEventData(event);
+    expect(notificationStore.addToast).toHaveBeenCalledWith(
+      'error',
+      'The event end cannot be before the event start'
+    );
+  });
 
-	it('error: publishDateTime after startDateTime based on publishDate', () => {
-		const event = {
-			startdate: '2025-09-08',
-			starttime: '13:00',
-			publishdate: '2025-10-01',
-			publishtime: '13:00',
-		};
-		// @ts-expect-error - Testing with incomplete event object
-		validateEventData(event);
-		expect(notificationStore.addToast).toHaveBeenCalledWith(
-			'error',
-			'The publish date cannot be after the start date'
-		);
-	});
+  it('error: endDateTime before startDateTime based on endtime', () => {
+    const event = {
+      startdate: '2025-09-09',
+      starttime: '13:00',
+      enddate: '2025-09-09',
+      endtime: '03:00',
+    };
+    // @ts-expect-error - Testing with incomplete event object
+    validateEventData(event);
+    expect(notificationStore.addToast).toHaveBeenCalledWith(
+      'error',
+      'The event end cannot be before the event start'
+    );
+  });
 
-	it('error: publishDateTime after startDateTime based on publishtime', () => {
-		const event = {
-			startdate: '2025-09-08',
-			starttime: '13:00',
-			publishdate: '2025-09-08',
-			publishtime: '18:00',
-		};
-		// @ts-expect-error - Testing with incomplete event object
-		validateEventData(event);
-		expect(notificationStore.addToast).toHaveBeenCalledWith(
-			'error',
-			'The publish date cannot be after the start date'
-		);
-	});
+  it('warning: endDateTime === startDateTime ', () => {
+    const event = {
+      startdate: '2025-09-09',
+      starttime: '13:00',
+      enddate: '2025-09-09',
+      endtime: '13:00',
+    };
+    // @ts-expect-error - Testing with incomplete event object
+    validateEventData(event);
+    expect(notificationStore.addToast).toHaveBeenCalledWith(
+      'warning',
+      'Both start and end date-time are the same'
+    );
+  });
+
+  it('error: publishDateTime after startDateTime based on publishDate', () => {
+    const event = {
+      startdate: '2025-09-08',
+      starttime: '13:00',
+      publishdate: '2025-10-01',
+      publishtime: '13:00',
+    };
+    // @ts-expect-error - Testing with incomplete event object
+    validateEventData(event);
+    expect(notificationStore.addToast).toHaveBeenCalledWith(
+      'error',
+      'The publish date cannot be after the start date'
+    );
+  });
+
+  it('error: publishDateTime after startDateTime based on publishtime', () => {
+    const event = {
+      startdate: '2025-09-08',
+      starttime: '13:00',
+      publishdate: '2025-09-08',
+      publishtime: '18:00',
+    };
+    // @ts-expect-error - Testing with incomplete event object
+    validateEventData(event);
+    expect(notificationStore.addToast).toHaveBeenCalledWith(
+      'error',
+      'The publish date cannot be after the start date'
+    );
+  });
+
+  it('error: unpublisDateTime before publishDateTime based un unpublishDate', () => {
+    const event = {
+      publishdate: '2025-04-04',
+      publishtime: '09:00',
+      unpublishdate: '2025-04-03',
+      unpublishtime: '09:00',
+    };
+    notificationStore.clear();
+    // @ts-expect-error - Testing with incomplete event object
+    validateEventData(event);
+    expect(notificationStore.addToast).toHaveBeenCalledWith(
+      'error',
+      'The unpublish date cannot be before the publish date'
+    );
+  });
+
+  it('error: unpublisDateTime before publishDateTime based un unpublishtime', () => {
+    const event = {
+      publishdate: '2025-04-03',
+      publishtime: '09:00',
+      unpublishdate: '2025-04-03',
+      unpublishtime: '05:00',
+    };
+    notificationStore.clear();
+    // @ts-expect-error - Testing with incomplete event object
+    validateEventData(event);
+    expect(notificationStore.addToast).toHaveBeenCalledWith(
+      'error',
+      'The unpublish date cannot be before the publish date'
+    );
+  });
+
+  it('warning: unpublisDateTime === publishDateTime', () => {
+    const event = {
+      publishdate: '2025-04-03',
+      publishtime: '09:00',
+      unpublishdate: '2025-04-03',
+      unpublishtime: '09:00',
+    };
+    notificationStore.clear();
+    // @ts-expect-error - Testing with incomplete event object
+    validateEventData(event);
+    expect(notificationStore.addToast).toHaveBeenCalledWith(
+      'warning',
+      'Both publish and unpublish date-time are the same.'
+    );
+  });
 });

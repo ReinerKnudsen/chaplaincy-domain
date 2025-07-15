4cbd5c5651c9f91db0cdd6480fa6ab2ec61dcd26
Reiner Knudsen <reinerknudsen@me.com>
2025-07-15 20:14:24 +0200

    images full-width on small screens && services in two-column grid on small screens

--- src/lib/components/ItemCard.svelte
+++ src/lib/components/ItemCard.svelte
@@ -22,7 +22,7 @@
 			<span class="headLineElement">{item.data.title}</span>
 		</div>
 		<div id="cardContent" class="justify-between py-4 md:flex md:gap-5">
-			<div class="mb-5 aspect-[3/2] h-52 overflow-hidden rounded-2xl">
+			<div class="mb-5 h-52 w-full overflow-hidden rounded-2xl md:aspect-[3/2] md:w-auto">
 				<img class="h-full w-full object-cover" src={item.data.image} alt={item.data.imageAlt} loading="lazy" />
 			</div>
 			<div id="cardText" class="text-md w-full md:w-[60%]">

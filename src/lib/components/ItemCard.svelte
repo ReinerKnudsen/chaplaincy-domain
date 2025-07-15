2be50792667170d4d59f8f1f54a87b30b0661e20
Reiner Knudsen <reinerknudsen@me.com>
2025-07-15 18:54:43 +0200

    itemcard is full-size link

--- src/lib/components/ItemCard.svelte
+++ src/lib/components/ItemCard.svelte
@@ -13,30 +13,32 @@
 	const itemCardContainer = '';
 </script>
 
-<div class="my-5 flex flex-col rounded-2xl border border-slate-200 bg-white p-6 shadow-xl">
-	<div id="itemHeadline" class="">
-		<a class="headLineLink" href="/{kind}/{item.id}">
+<a
+	class="card hover:bg-secondary-20 my-5 flex flex-col rounded-2xl border border-slate-200 bg-white p-6 shadow-xl hover:shadow-xl/20"
+	href="/{kind}/{item.id}"
+>
+	<div class="">
+		<div id="itemHeadline" class="headLineLink">
 			<span class="headLineElement">{item.data.title}</span>
-			<Icon class="headLineElement" icon="famicons:open-outline" />
-		</a>
-	</div>
-	<div id="cardContent" class="justify-between py-4 md:flex md:gap-5">
-		<div class="mb-5 aspect-[3/2] h-52 overflow-hidden rounded-2xl">
-			<img class="h-full w-full object-cover" src={item.data.image} alt={item.data.imageAlt} loading="lazy" />
 		</div>
-		<div id="cardText" class="text-md w-full md:w-[60%]">
-			<div class="mb-8">
-				{decodeHtml(item.data.slug)}
+		<div id="cardContent" class="justify-between py-4 md:flex md:gap-5">
+			<div class="mb-5 aspect-[3/2] h-52 overflow-hidden rounded-2xl">
+				<img class="h-full w-full object-cover" src={item.data.image} alt={item.data.imageAlt} loading="lazy" />
+			</div>
+			<div id="cardText" class="text-md w-full md:w-[60%]">
+				<div class="mb-8">
+					{decodeHtml(item.data.slug)}
+				</div>
+			</div>
+		</div>
+		<div id="cardFooter" class="flex min-h-5 flex-row justify-between">
+			<div class="text-primary-60 font-medium">{item.data.author}</div>
+			<div class="text-primary-60 font-medium md:justify-self-center">
+				{kind === 'news' ? item.data.publishdate : item.data.startdate}
 			</div>
 		</div>
 	</div>
-	<div id="cardFooter" class="flex min-h-5 flex-row justify-between">
-		<div class="text-primary-60 font-medium">{item.data.author}</div>
-		<div class="text-primary-60 font-medium md:justify-self-center">
-			{kind === 'news' ? item.data.publishdate : item.data.startdate}
-		</div>
-	</div>
-</div>
+</a>
 
 <style>
 </style>
